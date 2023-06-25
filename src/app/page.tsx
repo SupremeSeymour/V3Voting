fb0ea05a29e9f4efb38f9fecc8b2445dd492e275
SupremeSeymour <robyteoneoneone@gmail.com>
2023-06-24 19:41:25 -0700

    Approvals & Mint a Position Working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,36 @@
-import { Account } from '../components/Account'
-import { Connect } from '../components/Connect'
-import { Connected } from '../components/Connected'
-import { MintNFT } from '../components/MintNFT'
-import { NetworkSwitcher } from '../components/NetworkSwitcher'
+import { Account } from "../components/Account";
+import { Connect } from "../components/Connect";
+import { Connected } from "../components/Connected";
+import { Approval } from "../components/Approval";
 
 export function Page() {
   return (
     <>
-      <h1>wagmi + Next.js + @wagmi/cli (ABI)</h1>
+      <h1>V3 Voting</h1>
 
       <Connect />
 
       <Connected>
         <Account />
         <hr />
-        <MintNFT />
         <hr />
-        <NetworkSwitcher />
+        <Approval />
       </Connected>
     </>
-  )
+  );
 }
 
-export default Page
+export default Page;
+
+// Create random number and save somewhere as protoID -> give to ReadCreateHash -> browserID
+// With that result give it to StartSession along with large number(for time), browserID, time
+// To end Session -> give browserID, address, score
+// getCaptcha() func
+/* const handleSubmit = async () => {
+  const proof = await getProof("0xCC737a94FecaeC165AbCf12dED095BB13F037685", captchaText);
+  const proofHex = '0x' + proof.toString('hex');
+  onSubmit(421613, "0xB6E7d41064915D69fcb277001c8aAF61d087d9b4", 0, proofHex, userAddress);
+};*/
+// WriteonSubmitCaptcha(destinationID, ZkaptchaAddress, gas, proof, userAddress)
+// Wait for it 20-30s
+// Result
